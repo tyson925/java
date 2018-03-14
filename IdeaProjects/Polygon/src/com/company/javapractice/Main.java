ba8aa9e1b20ef5ab32ea101459f5c044cf5a2135
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-03-14 11:25:36 +0100

    Polygon finished

--- IdeaProjects/Polygon/src/com/company/javapractice/Main.java
+++ IdeaProjects/Polygon/src/com/company/javapractice/Main.java
@@ -1,15 +1,53 @@
+/*      1. Írj egy programot ami egy általad választott adatból kiszámítja egy tetraéder felszínét és térfogatát!
+        2. Írj egy programot ami egy általad választott adatból kiszámítja egy hexaéder felszínét és térfogatát!
+        3. Írj egy programot ami egy általad választott adatból kiszámítja egy oktaéder felszínét és térfogatát!
+        4. Írj egy programot ami egy általad választott adatból kiszámítja egy ikozaéder felszínét és térfogatát!
+        5. Írj egy programot ami egy általad választott adatból kiszámítja egy dodekaéder felszínét és térfogatát! */
+
 package com.company.javapractice;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        tetrahedron(11);
+        hexahedron(11);
+        octahedron(11);
+        dodecahedron(11);
+        icosahedron(11);
     }
 
-    public static void tetraeder(double a) {
-        double surface = Math.sqrt(a) * Math.pow(3, 2);
-        double volume = Math.(a) * h) / 3;
-        System.out.println("Surface of the tetraeder: " + surface);
-        System.out.println("Volume of the tetraeder: " + volume);
+    public static void tetrahedron(double a) {
+        double surface = Math.pow(a, 2) * Math.sqrt(3);
+        double volume = Math.pow(a, 3) * Math.sqrt(2) / 12;
+        System.out.println("Surface of the tetrahedron: " + Math.round(surface));
+        System.out.println("Volume of the tetrahedron: " + Math.round(volume));
+    }
+
+    public static void hexahedron(double a) {
+        double surface = 6* Math.pow(a, 2);
+        double volume = Math.pow(a, 3);
+        System.out.println("Surface of the hexahedron: " + Math.round(surface));
+        System.out.println("Volume of the hexahedron: " + Math.round(volume));
+    }
+
+    public static void octahedron(double a) {
+        double surface = Math.pow(a, 2) * 2 * Math.sqrt(3);
+        double volume = Math.pow(a, 2) * Math.sqrt(2) / 3;
+        System.out.println("Surface of the octahedron: " + Math.round(surface));
+        System.out.println("Volume of the octahedron: " + Math.round(volume));
+    }
+
+    public static void dodecahedron(double a) {
+        double surface = Math.pow(a, 2) * 3 * Math.sqrt(25 + 10 * Math.sqrt(5));
+        double volume = Math.pow(a, 2) * (15 + 7 * Math.sqrt(5)) / 4;
+        System.out.println("Surface of the dodecahedron: " + Math.round(surface));
+        System.out.println("Volume of the dodecahedron: " + Math.round(volume));
+    }
+
+    public static void icosahedron(double a) {
+        double surface = Math.pow(a, 2) * 5 * Math.sqrt(3);
+        double volume = Math.pow(a, 3) * (15 + 5 * Math.sqrt(5)) / 12;
+        System.out.println("Surface of the icosahedron: " + Math.round(surface));
+        System.out.println("Volume of the icosahedron: " + Math.round(volume));
     }
 }
